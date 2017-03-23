f1955498db41dec50cf85efe655c9b416472c38b
Will Hsu <will.g@mig.me>
2017-03-23 13:23:12 +0800

    modify: fix format

--- java_se_sample/src/com/will/ch_3/OperatorsPrioriy_3_1.java
+++ java_se_sample/src/com/will/ch_3/OperatorsPrioriy_3_1.java
@@ -4,12 +4,12 @@ public class OperatorsPrioriy_3_1 {
 
 	public static void main(String[] args) {
 		
-		boolean x = 5 * 3 < 20 & 3 + 7 > 9 - 1 || 20 >= 20 -30 && false;
+		boolean x = 5 * 3 < 20 & 3 + 7 > 9 - 1 || 20 >= 20 - 30 && false;
 		
 		
 		
 		
-		// x = 15 < 20 & 3 + 7 > 9 - 1 || 20 >= 20 -30 && false;
+		// x = 15 < 20 & 3 + 7 > 9 - 1 || 20 >= 20 - 30 && false;
 		// x = 15 < 20 & 20 > 8 || 20 >= -10 && false;
 		// x = true & true || true && false;
 		// x = true & true || true && false;
