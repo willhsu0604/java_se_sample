7baa17aa6a054c33cd25d685a4304307d12b0ba8
Will Hsu <will.g@mig.me>
2017-03-23 13:25:00 +0800

    add: shift operation sample

--- java_se_sample/src/com/will/ch_3/ShiftOperators_3_1.java
+++ java_se_sample/src/com/will/ch_3/ShiftOperators_3_1.java
@@ -13,6 +13,12 @@ public class ShiftOperators_3_1 {
 		System.out.println("a >> 1 : " + Integer.toBinaryString(a));
 		System.out.println("a = " + a);
 		
+		int b = 1;
+		System.out.println("b << 0 : " + (b << 0));
+		System.out.println("b << 1 : " + (b << 1));
+		System.out.println("b << 2 : " + (b << 2));
+		System.out.println("b << 3 : " + (b << 3));
+		System.out.println("b << 4 : " + (b << 4));
 	}
 	
 }
